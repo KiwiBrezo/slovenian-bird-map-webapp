cdf1e04b802e82d176b8aa8199d63ca425c5eca9
jBreznar <kiwi.breznar@gmail.com>
2020-12-18 17:11:18 +0100

    Init commit...

---
+++ src/main/java/com/sbm/slovenianbirdmap/SlovenianBirdMapApplication.java
@@ -0,0 +1,13 @@
+package com.sbm.slovenianbirdmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlovenianBirdMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlovenianBirdMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbm/slovenianbirdmap/SlovenianBirdMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbm.slovenianbirdmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlovenianBirdMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
