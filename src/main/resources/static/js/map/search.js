9f3959bd4262cb5df435dd018e4a20899fa79d74
jBreznar <kiwi.breznar@gmail.com>
2021-03-25 23:17:15 +0100

    Oblikovanje seznama za prikazovanje rezultatov.

--- src/main/resources/static/js/map/search.js
+++ src/main/resources/static/js/map/search.js
@@ -17,18 +17,40 @@
                 term: $("#search-observation-input").val()
             }
         }).done(function (response) {
-            console.log(response);
             $(".search-result-container .container-body").empty();
-            $.each(response || [], function (index, observation) {
-                var rowElement = $("<div>")
+            if ((response.length || []) > 0) {
+                $.each(response || [], function (index, observation) {
+                    var rowElement = $("<div>").addClass("result-info-container").append($("<div>")
+                            .addClass("row first-row"))
+                        .append($("<div>")
+                            .addClass("row second-row"))
 
-                rowElement.append($("<p>").text(observation.birdName));
+                    rowElement.find(".first-row").append($("<p>")
+                        .addClass("col-8 mx-1 bird-name-container")
+                        .text(observation.birdName));
 
-                $(".search-result-container .container-body").append(rowElement);
-            })
-            if (response != null) {
-                $(".search-result-container").show();
+                    rowElement.find(".second-row").append($("<p>")
+                        .addClass("col-8 mx-1 observer-name-container")
+                        .text("Opazovalec: " + observation.userName + " " + observation.userSurname));
+
+                    rowElement.find(".first-row").append($("<div>")
+                        .addClass("col-3")
+                        .css("text-align", "end")
+                        .append($("<i>")
+                            .addClass("fas fa-map-marked-alt cursor-pointer")));
+
+                    rowElement.find(".second-row").append($("<div>")
+                        .addClass("col-3")
+                        .css("text-align", "end")
+                        .append($("<i>")
+                            .addClass("fas fa-info-circle cursor-pointer")));
+
+                    $(".search-result-container .container-body").append(rowElement);
+                })
+            } else {
+                $(".search-result-container .container-body").append($("<p>").text("Ni bil najden noben zadetek!"));
             }
+            $(".search-result-container").show();
         });
     }
 
