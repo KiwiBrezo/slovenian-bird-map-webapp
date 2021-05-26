26f1548ff00b5cd6fe626a34f462136fee71dddc
jBreznar <kiwi.breznar@gmail.com>
2021-05-26 20:54:31 +0200

    Vklop samo potrbnih javascript modulov glede na stran.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,10 +5,33 @@
     }
 
     function initOther() {
-        MapComponent.init();
-        TestComponent.init();
-        UserController.init();
-        BirdInfoComponent.init();
-        BirdSearchComponent.init();
+        var controllerTypes = ($("#typeOfController").data("controller") || "none").split(",");
+        $.each(controllerTypes || [], function (index, val) {
+            switch (val) {
+                case "Map":
+                    MapComponent.init();
+                    break;
+                case "BirdSearch":
+                    BirdSearchComponent.init();
+                    break;
+                case "BirdInfo":
+                    BirdInfoComponent.init();
+                    break;
+                case "Test":
+                    TestComponent.init();
+                    break;
+                case "Main":
+                    // TODO: need main controller
+                    break;
+                case "User":
+                    UserController.init();
+                    break;
+                case "Admin":
+                    // TODO: need admin controller
+                    break;
+                default:
+                    console.log("No controller needed");
+            }
+        });
     }
 })(AppController = {})
