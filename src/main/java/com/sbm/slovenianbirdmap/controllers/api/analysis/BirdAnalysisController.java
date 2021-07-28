11e543b91f48e6b55551c87ddc9f4d0f55074155
jbreznar <kiwi.breznar@gmail.com>
2021-07-28 15:17:46 +0200

    Pridobivanje sql stavkov v javo in zacetek dela na analiticnem kontrolerju za ptice.

--- src/main/java/com/sbm/slovenianbirdmap/controllers/api/analysis/BirdAnalysisController.java
+++ src/main/java/com/sbm/slovenianbirdmap/controllers/api/analysis/BirdAnalysisController.java
@@ -1,6 +1,11 @@
 package com.sbm.slovenianbirdmap.controllers.api.analysis;
 
 import com.sbm.slovenianbirdmap.controllers.AbstractController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/analysis/bird")
 public class BirdAnalysisController extends AbstractController {
+    //TODO
 }
--- src/main/java/com/sbm/slovenianbirdmap/sql/ObservationSQL.java
+++ src/main/java/com/sbm/slovenianbirdmap/sql/ObservationSQL.java
@@ -19,6 +19,30 @@ public class ObservationSQL {
     @Value("${get.observation.for.user}")
     private String getObservationsForUser;
 
+    @Value("${find.last.10.observations.for.user}")
+    private String getLast10ObservationsForUser;
+
+    @Value("${weekly.observations.for.user}")
+    private String getWeeklyObservationsForUser;
+
+    @Value("${monthly.observation.for.user}")
+    private String getMonthlyObservationsForUser;
+
+    @Value("${number.of.birds.observed}")
+    private String getNumberOfBirdsObserved;
+
+    @Value("${number.of.observers.for.bird}")
+    private String getNumberOfObserversForBird;
+
+    @Value("${monthly.observation.for.bird}")
+    private String getMonthlyObservationsForBird;
+
+    @Value("${yearly.observation.for.bird}")
+    private String getYearlyObservationForBird;
+
+    @Value("${season.observation.for.bird}")
+    private String getSeasonObservationForBird;
+
     public String getGetObservationsForUser() { return getObservationsForUser; }
 
     public String getSearchObservationDIstinctBirdIDs() { return searchObservationDIstinctBirdIDs; }
@@ -28,4 +52,20 @@ public class ObservationSQL {
     public String getAddNewObservation() {
         return addNewObservation;
     }
+
+    public String getGetLast10ObservationsForUser() { return getLast10ObservationsForUser; }
+
+    public String getGetWeeklyObservationsForUser() { return getWeeklyObservationsForUser; }
+
+    public String getGetMonthlyObservationsForUser() { return getMonthlyObservationsForUser; }
+
+    public String getGetNumberOfBirdsObserved() { return getNumberOfBirdsObserved; }
+
+    public String getGetNumberOfObserversForBird() { return getNumberOfObserversForBird; }
+
+    public String getGetMonthlyObservationsForBird() { return getMonthlyObservationsForBird; }
+
+    public String getGetYearlyObservationForBird() { return getYearlyObservationForBird; }
+
+    public String getGetSeasonObservationForBird() { return getSeasonObservationForBird; }
 }
